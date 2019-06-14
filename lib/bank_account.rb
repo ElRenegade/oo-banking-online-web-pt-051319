50d6ae21953fd5e54737d443e971e8354897f2a3
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-14 19:08:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,7 +17,7 @@ class BankAccount
   end
 
   def valid?
-    if self.balance > 0 && slef.status == "open"
+    if self.balance > 0 && self.status == "open"
       return true
     else
       return false
